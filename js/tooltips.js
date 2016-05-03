651907e12370076a27f198e44f249ebb80442fa4
Andre Dezain <Andre Dezain>
2016-05-03 10:20:04 +0700

    commit1

---
+++ js/tooltips.js
@@ -0,0 +1,10 @@
+$(function() {
+	if ($.browser.msie && $.browser.version.substr(0,1)<7)
+	{
+		$('.tooltip').mouseover(function(){
+			$(this).children('span').show();
+		}).mouseout(function(){
+			$(this).children('span').hide();
+		})
+	}
+});
---
+++ readme.txt
@@ -0,0 +1,5 @@
+old toska: 009980
+young toska: 04b99b
+black: 333
+grey: ddd
+white: fff 
